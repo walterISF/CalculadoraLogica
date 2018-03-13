71525429415dd39ecc65b424a1ffe5837ce2182c
calebecarvalho <yoshke23@gmail.com>
2018-03-13 20:46:12 -0300

    Implementação pilha sem validações

--- src/calculadoralogica/Pilha.java
+++ src/calculadoralogica/Pilha.java
@@ -5,10 +5,41 @@
  */
 package calculadoralogica;
 
+import java.util.*;
+
 /**
  *
- * @author vntwafi
+ * @author Calebe
  */
 public class Pilha {
     
+    private int tamMax;
+    private String[] pilha;
+    private int topo;
+    
+    Pilha(int tam){
+        tamMax = tam;
+        pilha = new String[tamMax];
+        topo = -1;
+    }
+    
+    public void adcionar(String elem){
+        pilha[++topo] = elem;
+    }
+    
+    public String retirar(){
+        return pilha[topo--];
+    }
+    
+    public String acessar(){
+        return pilha[topo];
+    }
+    
+    public Boolean vazia(){
+        return (topo == -1);
+    }
+    
+    public Boolean cheia(){
+        return (topo == tamMax -1);
+    }
 }
