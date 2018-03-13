7ef4eba35f6fddc952cea6369140fb44b3487c23
Walter <walter.filho@venturus.org.br>
2018-03-12 21:36:03 -0300

    forst commit on error

---
+++ src/calculadoralogica/Fila.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoralogica;
+
+/**
+ *
+ * @author Calebe Carvalho
+ */
+public class Fila {
+
+    int inicio=0;
+    int fim=-1;
+    int qtd=0;
+
+    Fila (int capacidade, String[] item) throws Exception
+    {
+
+        if(capacidade<=0)
+            throw new Exception("Valor invalido");
+
+        item = new String[capacidade];
+    }
+
+    void inserir(String elemento, String[] item) throws Exception
+    {
+        if(elemento==null)
+            throw new Exception("Insira umm valor");
+
+        if(qtd==item.length)
+            throw new Exception("Fila cheia");
+
+        fim++;
+        if(fim==item.length)
+            fim=0;
+
+        item[fim] = elemento;
+        qtd++;
+
+    }
+
+    String retornarElemento()
+    {
+        //return item[fim];
+        return null;
+    }
+
+    String removerElemento(String[] item) throws Exception
+    {
+        if(qtd==0)
+        throw new Exception("Fila vazia");
+
+        item[fim] = null;
+        qtd--;
+
+        if(fim==item.length)
+            fim=0;
+        qtd--;
+        
+        return null;
+
+    }
+
+}
