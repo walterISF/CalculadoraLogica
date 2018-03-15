aac1e0779c40271e1e91456a8f6229e3fec4a94c
Tadeu <>
2018-03-15 19:43:58 -0300

    edicao do main

--- src/calculadoralogica/CalculadoraLogica.java
+++ src/calculadoralogica/CalculadoraLogica.java
@@ -3,7 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
 package calculadoralogica;
+
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 /**
  *
@@ -14,8 +21,20 @@ public class CalculadoraLogica {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception
+    {
+        /**
+         * Tadeu, teste
+         */
+        
+        BufferedReader lerTeclado = new BufferedReader (new InputStreamReader (System.in)); //variavel que irá ler o que o usuario digitou e guardar
+        
+        
+        String vetorExpressao = new String(); //string da expressão logica
+        
+        System.out.print("Digite uma expressão lógica: ");
+        vetorExpressao = lerTeclado.readLine();
+        
     }
     
 }
