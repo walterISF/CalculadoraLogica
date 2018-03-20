7c6764658c8d348390804000b2631a9fdb4915f2
calebecarvalho <yoshke23@gmail.com>
2018-03-20 17:38:17 -0300

    Nova classe

---
+++ src/calculadoralogica/ConversorPosFixa.java
@@ -0,0 +1,27 @@
+package calculadoralogica;
+
+import java.io.*;
+import calculadoralogica.Fila;
+import calculadoralogica.Pilha;
+
+public class ConversorPosFixa {
+    
+    private String expressao;
+    private String[] partes;
+    Fila filaSaida;
+    Pilha pilhaOperadores;
+        
+ConversorPosFixa(String expressao)
+{
+ this.expressao = expressao;
+}
+
+public void Converte()
+{
+  
+
+}
+    
+}
+
+
